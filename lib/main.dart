c1ca656822cc0e226b6f1e7194885f8631bc7408
ROSHAN <roshanw1998@gmail.com>
2021-11-03 11:26:10 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:complete_authentication_tut/bloc/login_bloc.dart';
+import 'package:complete_authentication_tut/bloc/register_bloc.dart';
+import 'package:complete_authentication_tut/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<LoginBloc>(create: (context) => LoginBloc()),
+        Provider<RegisterBloc>(create: (context) => RegisterBloc())
+      ],
+      child: MaterialApp(
+        title: 'Complete Authentication',
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
+      ),
+    );
+  }
+}
